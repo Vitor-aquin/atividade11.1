4dc05e51acc4b7bab0b320e17c9d298095b042ef
Vitor-aquin <vitormktd2802@gmail.com>
2024-10-28 17:37:09 -0300

    Add files via upload

---
+++ atividade11.1.py
@@ -0,0 +1,11 @@
+#Cria uma lista chamada 'numeros' com os números de 1 a 5
+numeros = list(range(1, 6))
+
+# Loop para percorrer cada número na lista
+for numero in numeros:
+
+   
+# Mostra o número atual
+
+ print(numero)   
+   
